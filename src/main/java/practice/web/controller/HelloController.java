7b320a0de81895686dd498c925049ec58ba9aa35
Jealyang <jaelyangchoi@gmail.com>
2021-04-18 18:04:16 +0900

    동적 컨텐츠 전달. MVC를 거친다.

--- src/main/java/practice/web/controller/HelloController.java
+++ src/main/java/practice/web/controller/HelloController.java
@@ -3,6 +3,7 @@ package practice.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // 톰캣이 Controller에게 요청 처리하라 함
 // model에 값을 집어넣고
@@ -13,5 +14,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello~!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) { // RequestParam - required: default True
+        model.addAttribute("name", name);
+        return "mvc-template";
     }
 }
