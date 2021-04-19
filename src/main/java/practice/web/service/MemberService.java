150b131c92fc8fab4a23cba38478e1ae9206ae23
Jealyang <jaelyangchoi@gmail.com>
2021-04-19 23:19:43 +0900

    회원 관련 서비스 클래스 구현

---
+++ src/main/java/practice/web/service/MemberService.java
@@ -0,0 +1,39 @@
+package practice.web.service;
+
+import practice.web.domain.Member;
+import practice.web.repository.MemberRepository;
+import practice.web.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); //같은 이름이 있는 중복 회원x
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 이름입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
