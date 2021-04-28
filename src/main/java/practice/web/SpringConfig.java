59c332fd0b86db2cf72c3b60a517efd3e1d281f3
Jealyang <jaelyangchoi@gmail.com>
2021-04-28 15:59:19 +0900

    순수 jdbc를 이용한 db 연결 구현

--- src/main/java/practice/web/SpringConfig.java
+++ src/main/java/practice/web/SpringConfig.java
@@ -1,13 +1,26 @@
 package practice.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import practice.web.repository.JdbcMemberRepository;
 import practice.web.repository.MemberRepository;
 import practice.web.repository.MemoryMemberRepository;
 import practice.web.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+    /*DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체.
+    스프링 부트는 application.properties의 데이터베이스 커넥션 정보를 바탕으로 DataSource를 생성하고 스프링 빈으로 만들어둔다.
+    그래서 DI를 받을 수 있다.*/
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +30,7 @@ public class SpringConfig {
 
     @Bean //singleton으로 동작
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
